e74a168c70f6e925ef54e95a80201edcd52cb3f7
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-05-20 16:03:01 +0530

    server configuration Initialized

---
+++ server/propertylisting/src/main/java/com/shantanu/propertylisting/PropertylistingApplication.java
@@ -0,0 +1,13 @@
+package com.shantanu.propertylisting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertylistingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertylistingApplication.class, args);
+	}
+
+}
---
+++ server/propertylisting/src/test/java/com/shantanu/propertylisting/PropertylistingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shantanu.propertylisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertylistingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
