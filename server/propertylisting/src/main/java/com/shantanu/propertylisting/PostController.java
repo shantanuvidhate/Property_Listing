fcef623d04bed996a8e7c4badaa612bc859d2023
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-05-21 11:32:40 +0530

    application properties changed and initialized swagger-ui.html

--- server/propertylisting/src/main/java/com/shantanu/propertylisting/PostController.java
+++
@@ -1,8 +0,0 @@
-package com.shantanu.propertylisting;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class PostController {
-
-}
---
+++ server/propertylisting/src/main/java/com/shantanu/propertylisting/controller/PostController.java
@@ -0,0 +1,18 @@
+package com.shantanu.propertylisting.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+public class PostController {
+    @ApiIgnore
+    @RequestMapping(value="/")
+    public void redirect (HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
+    }
+
+}
