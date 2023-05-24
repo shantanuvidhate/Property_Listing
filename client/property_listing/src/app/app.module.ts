b5c682eeff503924e6928bc2711784da6342e137
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-05-24 05:46:12 -0500

    Implemented landing banner

--- client/property_listing/src/app/app.module.ts
+++ client/property_listing/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { LandingComponent } from './components/landing/landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
