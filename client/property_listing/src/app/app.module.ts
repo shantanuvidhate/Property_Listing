eeaa08b6cfec20ea2c94e9763f2534405cdac87c
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-05-24 05:57:57 -0500

    structure changed

--- client/property_listing/src/app/app.module.ts
+++ client/property_listing/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { LandingComponent } from './components/landing/landing/landing.component';
+import { SearchComponent } from './components/search/search.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    SearchComponent,
     LandingComponent
   ],
   imports: [
