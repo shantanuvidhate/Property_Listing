cbfd013197d0e4dc3a891107c2d14690b30049fb
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-05-26 14:54:06 +0530

    Location selection

--- client/property_listing/src/app/components/search/drop-location/drop-location.component.ts
+++ client/property_listing/src/app/components/search/drop-location/drop-location.component.ts
@@ -7,14 +7,13 @@ import { Component } from '@angular/core';
     <button mat-button [matMenuTriggerFor]="menu" class="dropdown-btn w-full text-left" (click)="toggleExpand()">
        <mat-icon class="dropdown-icon-primary">location_on</mat-icon>
     <div>
-      <div class="text-[15px] font-medium leading-tight">Location</div>
+      <div class="text-[15px] font-medium leading-tight">{{selectedLocation ? selectedLocation : 'Location' }}</div>
       <div class="text-[15px]">Select Location</div>
     </div>
     <mat-icon class="dropdown-icon-secondary"> {{isExpanded ? 'expand_less' : 'keyboard_arrow_down'}} </mat-icon>
   </button>
     <mat-menu #menu="matMenu" class="cursor-pointer hover:text-white-700">
-      <button mat-menu-item>California</button>
-      <button mat-menu-item>Santa Barbra</button>
+      <button mat-menu-item *ngFor="let location of locations" (click)="selectLocation(location)">{{location}}</button>
     </mat-menu>
   </Menu>
 
@@ -25,9 +24,15 @@ import { Component } from '@angular/core';
 export class DropLocationComponent {
 
   isExpanded: boolean = false;
+  locations :string[] = ['California' , 'Austin' , 'Santa Clara'];      // ------> later to be get by API on ngOninit
+  selectedLocation : string | null= null;
 
   toggleExpand(): void {
     this.isExpanded = !this.isExpanded;
+  }
+
+  selectLocation (location:string) : void {
+    this.selectedLocation = location;
   }
 
 }
