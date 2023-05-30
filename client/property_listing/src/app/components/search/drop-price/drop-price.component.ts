f082d586e4ca642a43acda52c023476ff71e1fd0
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-05-30 23:40:19 +0530

    Price range toggle

--- client/property_listing/src/app/components/search/drop-price/drop-price.component.ts
+++ client/property_listing/src/app/components/search/drop-price/drop-price.component.ts
@@ -3,11 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-drop-price',
   template: `
-
+    <Menu as='div' class="dropdown relative border border-slate-400 rounded-lg">
+    <button mat-button [matMenuTriggerFor]="menu" class="dropdown-btn w-full text-left" (click)="toggleExpand()">
+       <mat-icon class="dropdown-icon-primary">account_balance_wallet</mat-icon>
+    <div>
+      <div class="text-[15px] font-medium leading-tight">{{selectedPriceRange ? selectedPriceRange : 'Price' }}</div>
+      <div class="text-[15px]">Price Range </div>
+    </div>
+    <mat-icon class="dropdown-icon-secondary"> {{isExpanded ? 'expand_less' : 'keyboard_arrow_down'}} </mat-icon>
+  </button>
+    <mat-menu #menu="matMenu" class="cursor-pointer hover:text-white-700">
+      <button (click)="toggleExpand()" mat-menu-item *ngFor="let priceRange of priceRanges" (click)="selectPriceRange(priceRange)">{{priceRange}}</button>
+    </mat-menu>
+  </Menu>
   `,
   styles: [
   ]
 })
 export class DropPriceComponent {
+  
+  isExpanded: boolean = false;
+  priceRanges :string[] = [];      
+  selectedPriceRange : string | null= null;
 
+  toggleExpand(): void {
+    this.isExpanded = !this.isExpanded;
+  }
+
+  selectPriceRange (priceRange:string) : void {
+    this.selectedPriceRange = priceRange;
+  }
 }
