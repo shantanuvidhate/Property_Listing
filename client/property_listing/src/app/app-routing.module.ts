ff0a22347a5e0acd34b5bc244eb96be271eae6ee
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-06-07 00:58:42 +0530

    Dynamic Routing with with id as params

--- client/property_listing/src/app/app-routing.module.ts
+++ client/property_listing/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PropertyDetailsComponent } from './pages/property-details/property-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component:PropertyDetailsComponent,
+    path:'propertyDetail/:id'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
