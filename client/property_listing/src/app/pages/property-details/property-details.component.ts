9c05d77a246b716360ba7dc46d02b21c5e7f2917
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-06-08 18:45:03 +0530

    form component in property details

--- client/property_listing/src/app/pages/property-details/property-details.component.ts
+++ client/property_listing/src/app/pages/property-details/property-details.component.ts
@@ -11,10 +11,18 @@ export class PropertyDetailsComponent implements OnInit {
 constructor (private _activatedRoutes : ActivatedRoute){}
 
   id:any;
+  fullName : string = 'Tom Holland'
 
   ngOnInit(): void {
     this.id = this._activatedRoutes.snapshot.paramMap.get('id');
     console.log(this.id)
+  }
+
+  getNameIcon (): string {
+    const names = this.fullName.split(' ');
+    const IconNames = names.map(name => name.charAt(0).toUpperCase());
+    console.log('Hi');
+    return IconNames.join('');
   }
 
 
