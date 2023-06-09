8b4cdfa10513212bf5f5383b40f8b21ca427726c
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-06-09 23:50:36 +0530

    Data rendered in property-detail HTML component

--- client/property_listing/src/app/pages/property-details/property-details.component.ts
+++ client/property_listing/src/app/pages/property-details/property-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { PropertyListService } from 'src/app/services/property-list.service';
+import { PropertyDetails } from 'src/app/components/utilities/propertyDetails';
 
 @Component({
   selector: 'app-property-details',
@@ -16,20 +17,32 @@ export class PropertyDetailsComponent implements OnInit {
     ){}
   
   id:any;
-  fullName : string = 'Tom Holland';
   contactForm:any;
-  data:any;
-
-
+  propertyDetail: PropertyDetails = {
+    id: 0,
+    title: '',
+    price: 0,
+    location: '',
+    country: '',
+    area: 0,
+    bedrooms: 0,
+    bathrooms: 0,
+    status: '',
+    description: '',
+    image_url: '',
+    owner_name:''
+  };
+  
+  
   ngOnInit(): void {
     this.id = this._activatedRoutes.snapshot.paramMap.get('id');
-    console.log(this.id)
-
+    // console.log(this.id)
+    
     this._propertyService.getPropertyDetailsById(this.id).subscribe(response =>{
-      this.data = response;
+      this.propertyDetail = response;
       console.log(response)
     });
-
+    
     this.contactForm = new FormGroup ({
       name: new FormControl('' , [Validators.required]),
       email: new FormControl('' , [Validators.required , Validators.email]),
@@ -38,10 +51,10 @@ export class PropertyDetailsComponent implements OnInit {
     })
   }
 
-  getNameIcon (): string {
-    const names = this.fullName.split(' ');
+  getNameIcon (owner_name : string): string {
+    const names = owner_name.split(' ');
     const IconNames = names.map(name => name.charAt(0).toUpperCase());
-    console.log('Hi');
+    // console.log('Hi');
     return IconNames.join('');
   }
 
